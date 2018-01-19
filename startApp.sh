43c4dddae671b64dd0809056d3494ad2d774c30c
Eric Bergstedt <Eric.Bergstedt@deepsea.com>
2018-01-19 10:05:40 -0800

    command updates

--- startApp.sh
+++ startApp.sh
@@ -1,3 +1,10 @@
+
+cd /home/pi/Application/DSPL-serial/
+
+sudo chmod +x update.sh
+sudo chmod +x ARM_xbox_serial
+sudo chmod +x Controller_GUI
+
 sudo sh update.sh
 ./ARM_xbox_serial &
 ./Controller_GUI &
