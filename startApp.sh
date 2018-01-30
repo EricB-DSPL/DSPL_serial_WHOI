126a13c3c29f915fc6cb3016b4177b7be6a01ea6
Eric Bergstedt <Eric.Bergstedt@deepsea.com>
2018-01-30 09:54:57 -0800

    Changed permissions to settings.json to allow rewrite access

--- startApp.sh
+++ startApp.sh
@@ -4,6 +4,7 @@ cd /home/pi/Application/DSPL-serial/
 sudo chmod +x update.sh
 sudo chmod +x ARM_xbox_serial
 sudo chmod +x Controller_GUI
+sudo chmod +rw settings.json
 
 sudo sh update.sh
 ./ARM_xbox_serial &
