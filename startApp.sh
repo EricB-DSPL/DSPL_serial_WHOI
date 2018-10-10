86305cae513c17b45abb9bd047b2dd28715a07e6
Eric Bergstedt <eric.bergstedt@deepsea.com>
2018-10-10 14:46:01 -0700

    Adding support for USR604 B&B

--- startApp.sh
+++ startApp.sh
@@ -1,3 +1,5 @@
+sudo modprobe fdio_sio
+sleep 1
 
 cd /home/pi/Application/DSPL-serial/
 
@@ -5,6 +7,9 @@ sudo chmod +x update.sh
 sudo chmod +x ARM_xbox_serial
 sudo chmod +x Controller_GUI
 sudo chmod 777 settings.json
+
+sudo chmod 777 /sys/bus/usb-serial/ftdi_sio/new_id
+sudo echo "0856 bf02" > /sys/bus/usb-serial/ftdi_sio/new_id
 
 sudo sh update.sh
 ./ARM_xbox_serial &
